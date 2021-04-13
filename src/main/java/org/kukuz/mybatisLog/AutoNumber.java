ac6bc1c40e7edfd42eacb94b0b252ffee5598cf9
luhangqi <1351395043@qq.com>
2021-04-13 17:10:55 +0800

    兼容211

---
+++ src/main/java/org/kukuz/mybatisLog/AutoNumber.java
@@ -0,0 +1,18 @@
+package org.kukuz.mybatisLog;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * 自动生成序号
+ *
+ * @author luhangqi
+ * @date 2021/3/5
+ */
+public class AutoNumber extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        
+    }
+}
